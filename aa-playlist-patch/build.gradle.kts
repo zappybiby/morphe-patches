296d16e5da80b190f81345100f48ad5a47a7feb0
brian <blittle65@gmail.com>
2026-08-15 08:32:32 -0400

    fix: Allow Manager to inject the standalone extension

--- aa-playlist-patch/build.gradle.kts
+++ aa-playlist-patch/build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 apply<app.morphe.patches.gradle.PatchesPlugin>()
 
 group = "app.morphe"
-version = "0.1.1"
+version = "0.1.2"
 
 extensions.configure<PatchesExtension>("patches") {
     about {
