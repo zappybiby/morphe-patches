2f6e98c3ae09ad267c789ef6e20fdf6b01db6d65
brian <blittle65@gmail.com>
2026-08-17 13:25:37 -0400

    build: Prepare playable playlist release 0.0.7

--- aa-playlist-patch/build.gradle.kts
+++ aa-playlist-patch/build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 apply<app.morphe.patches.gradle.PatchesPlugin>()
 
 group = "app.morphe"
-version = "0.0.6"
+version = "0.0.7"
 
 extensions.configure<PatchesExtension>("patches") {
     about {
