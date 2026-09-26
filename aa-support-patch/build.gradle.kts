d5825202d832b0d7957087da87539bd7be3e39e7
brian <blittle65@gmail.com>
2026-09-25 22:57:42 -0400

    build: Package Android Auto support release 0.4.0
    Package SupportAndroidAutoPatch and its extension as a standalone MPP bundle.

---
+++ aa-support-patch/build.gradle.kts
@@ -0,0 +1,57 @@
+import app.morphe.patches.gradle.PatchesExtension
+import org.gradle.jvm.tasks.Jar
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+
+apply<app.morphe.patches.gradle.PatchesPlugin>()
+
+group = "app.morphe"
+version = "0.4.0"
+
+extensions.configure<PatchesExtension>("patches") {
+    about {
+        name = "YouTube Music Android Auto support"
+        description = "Supports YouTube Music playlists and podcasts in Android Auto"
+        source = "https://github.com/zappybiby/morphe-patches"
+        author = "zappybiby"
+        contact = "na"
+        website = "https://github.com/zappybiby/morphe-patches"
+        license = "GNU General Public License v3.0, with additional GPL section 7 requirements"
+    }
+}
+
+dependencies {
+    add("implementation", libs.guava)
+    add("implementation", libs.morphe.patches.library)
+    add("compileOnly", project(":patches:stub"))
+}
+
+extensions.configure<KotlinJvmProjectExtension>("kotlin") {
+    compilerOptions {
+        freeCompilerArgs.set(listOf("-Xcontext-parameters"))
+    }
+    sourceSets {
+        named("main") {
+            kotlin.srcDir("../patches/src/main/kotlin")
+            kotlin.include(
+                "app/morphe/patches/music/misc/androidauto/support/Fingerprints.kt",
+                "app/morphe/patches/music/misc/androidauto/support/SupportAndroidAutoPatch.kt",
+                "app/morphe/patches/music/shared/Constants.kt",
+            )
+        }
+    }
+}
+
+tasks.withType<Jar>().configureEach {
+    archiveBaseName.set("support-android-auto")
+    exclude(
+        "extensions/music.mpe",
+        "extensions/reddit.mpe",
+        "extensions/shared.mpe",
+        "extensions/shared-youtube.mpe",
+        "extensions/youtube.mpe",
+    )
+}
+
+tasks.named("build") {
+    dependsOn("buildAndroid")
+}
--- patches/src/main/kotlin/app/morphe/patches/music/misc/androidauto/support/SupportAndroidAutoPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/music/misc/androidauto/support/SupportAndroidAutoPatch.kt
@@ -17,7 +17,6 @@ import app.morphe.patcher.patch.bytecodePatch
 import app.morphe.patcher.util.proxy.mutableTypes.MutableClass
 import app.morphe.patcher.util.proxy.mutableTypes.MutableMethod.Companion.toMutable
 import app.morphe.patcher.util.smali.ExternalLabel
-import app.morphe.patches.music.misc.extension.sharedExtensionPatch
 import app.morphe.patches.music.shared.Constants.COMPATIBILITY_YOUTUBE_MUSIC
 import app.morphe.util.cloneMutable
 import app.morphe.util.findFreeRegister
@@ -81,7 +80,7 @@ val supportAndroidAutoPatch = bytecodePatch(
     name = "Restore playlists and podcasts in Android Auto",
     description = "Restores YouTube Music playlists and podcasts in Android Auto.",
 ) {
-    dependsOn(sharedExtensionPatch)
+    extendWith("extensions/android-auto-support.mpe")
 
     compatibleWith(COMPATIBILITY_YOUTUBE_MUSIC)
 
